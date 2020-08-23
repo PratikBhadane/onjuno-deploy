45b4885860f52b34c98b403f0bb7e09c0b27402f
Pratik Bhadane <pratikbhadane100@gmail.com>
2020-08-23 17:51:35 +0530

    Updates

---
+++ precache-manifest.d4da2906fdfd78fef54f67beff984028.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4a013a9d7c77f11321571a222fd503",
+    "url": "/onjuno-deploy/index.html"
+  },
+  {
+    "revision": "0326a1afc1a846ff1456",
+    "url": "/onjuno-deploy/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d38831f5c37697cfc8d7",
+    "url": "/onjuno-deploy/static/css/main.e8f9de1a.chunk.css"
+  },
+  {
+    "revision": "0326a1afc1a846ff1456",
+    "url": "/onjuno-deploy/static/js/2.4a225416.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/onjuno-deploy/static/js/2.4a225416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38831f5c37697cfc8d7",
+    "url": "/onjuno-deploy/static/js/main.8e4de1f6.chunk.js"
+  },
+  {
+    "revision": "4b5578602c0e727cebb5",
+    "url": "/onjuno-deploy/static/js/runtime-main.6f2430ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/onjuno-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
